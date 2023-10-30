ec0c2e90b56ad01d952b4989a17b50bb1278ea35
kaushikchoudhury <kaushikc44@gmail.com>
2023-10-31 01:53:26 +0530

    chanegs to layout

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Wallet from "../components/Wallet"
 
-export default function Home() {
+export default function Home({children}) {
   return (
     <>
+    <Wallet >
+      {children}
+    </Wallet>
     </>
   )
 }
